be3a72e1340014b68f5690c4109724653f7fec97
Shubham Shiroya <shiroyashubham1@gmail.com>
2023-11-27 22:28:21 +0530

    Fist Commit

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Button from './Button'
+import Input from './Input'
+import RTE from "./RTE";    
+import Signup from './Signup'
+import Login from "./Login";
+import PostForm from "./postForm/PostForm";
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+import Select from './Select'
+
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, RTE, Signup, Login, PostForm, PostCard, AuthLayout, Select }
